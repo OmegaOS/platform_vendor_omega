b4d6e989bf1131e6537d8751c42ff43c55d4a023
Steve Kondik <steve@cyngn.com>
2015-12-01 13:31:45 -0800

    cm: Include librsjni explicitly
     * Make sure this gets included since GMS webview breaks the dependency.
    
    Change-Id: I49b01309dce8d9779ca58576c2d7ee78d133ddb0

--- common.mk
+++ common.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
     SnapdragonGallery \
     SnapdragonMusic
 
+# Include librsjni explicitly to workaround GMS issue
+PRODUCT_PACKAGES += \
+    librsjni
+
 # Mms depends on SoundRecorder for recorded audio messages
 PRODUCT_PACKAGES += \
     SoundRecorder
